23b1cc1c289ba88daf950a92228b058d5671441e
Adam Gregory <agregory@gmail.com>
2015-08-02 10:28:10 -0500

    initial commit

---
+++ client/js/app.js
@@ -0,0 +1,28 @@
+$('.jstest').html('Hello World!');
+
+console.log($)
+console.log('test')
+
+function get_track_info() {
+
+    $.ajax('/track-info', {
+        //cache: false,
+        dataType: 'json',
+        success: function (data) {
+        	console.log(data)
+            if (data.title) {
+                $('#live-info .album-art').attr('src', data.art.replace('100x100', '400x400'));
+                $('#live-info .album-art').attr('alt', data.album);
+                $('#live-info .album-art').attr('title', data.album);
+                $('#live-info .artist').html(data.artist);
+                $('#live-info .title').html(data.title);
+            }
+        },
+        error: function (data) {
+        	console.log('error',data);
+        }
+    });
+    setTimeout(function(){get_track_info();}, 200000);
+}
+
+get_track_info();
---
+++ server/app.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var request = require('request');
+var path = require('path');
+var app = express();
+
+var bodyParser = require('body-parser');
+//app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
+
+app.use(express.static(path.join(__dirname, '../client')));
+
+app.get('/track-info', function (req, res) {
+
+  var options = {
+	url: 'http://www.thecurrent.org/playlist/metadata/current'
+	}
+
+	request(options, function(err, response, body) {
+		if (err) return res.send(err);
+		
+		res.send(body);
+	});
+
+});
+
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
